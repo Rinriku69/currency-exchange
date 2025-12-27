03dba42d3da18d7f4cad84cbea58d08ad32873a9
Rinriku69 <siripoohkim@gmail.com>
2025-12-27 16:29:59 +0700

    Refactor Dashboard polling logic and cleanup
    Removed OnInit and OnDestroy lifecycle hooks and refactored polling logic to use a setPolling method in the currency state. Updated LiveSwitch to properly control polling state and removed unused imports and signals.

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -9,37 +9,25 @@ import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [AsyncPipe, CurrencyCard],
+  imports: [CurrencyCard],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard implements OnInit, OnDestroy {
+export class Dashboard {
   private currencyState = inject(Currency);
   dashboard: Signal<DashBoard[]> = toSignal(this.currencyState.CurrentCurrenciesWithTrend$,
     { initialValue: [] })
   private destroyed$ = new Subject<void>;
   private destroyRef = inject(DestroyRef);
   isPolling = signal(false);
+  data = toSignal(this.currencyState.updates$);
 
   constructor() {
     this.currencyState.getDashboard()
   }
 
-  ngOnInit(): void {
-    timer(0, 5000).pipe(
-      takeUntil(this.destroyed$),
-      filter(() => this.isPolling())
-    ).subscribe(() =>
-      this.currencyState.RandomRate())
-  }
-
-  ngOnDestroy(): void {
-    console.log('Dashboard destroyed');
-    this.destroyed$.next()
-    this.destroyed$.complete()
-  }
-
-  LiveSwitch(bool: boolean) {
-    this.isPolling = signal(bool)
+  LiveSwitch(isActive: boolean) {
+    this.isPolling.set(isActive)
+    this.currencyState.setPolling(isActive);
   }
 }
