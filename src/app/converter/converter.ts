f0704ea8ac02958a779c89f5c6ac068e253f696e
Rinriku69 <siripoohkim@gmail.com>
2025-12-28 22:29:04 +0700

    refactor to signal based within compoenent

--- src/app/converter/converter.ts
+++ src/app/converter/converter.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject, Input, signal, Signal } from '@angular/core';
+import { Currency } from '../../services/currency';
+import { DashBoard } from '../../model/dashboard';
+import { toSignal } from '@angular/core/rxjs-interop';
+
 
 @Component({
   selector: 'app-converter',
@@ -7,5 +11,36 @@ import { Component } from '@angular/core';
   styleUrl: './converter.scss',
 })
 export class Converter {
+  currencyState = inject(Currency)
+  dashboard: Signal<DashBoard[]> = toSignal(this.currencyState.CurrentCurrencies$, { initialValue: [] })
+  inputCode = this.dashboard().map(currnecy =>
+    currnecy.currencyCode
+  )
+  inputUnit = signal('JPY')
+  outputUnit = signal('THB')
+  inputValue = signal(0)
+  outputCode: Signal<string[]> = computed(() => {
+    return this.inputCode
+  })
+  convertedValue = toSignal(this.currencyState.convertedValue$, { initialValue: 0 })
+
+  constructor() {
+    effect(() => {
+      this.currencyState.toConvert$.next({ value: this.inputValue(), from: this.inputUnit(), to: this.outputUnit() })
+    })
+
+  }
+  inputUnitChange(unit: string) {
+    this.inputUnit.set(unit)
+
+  }
+  outputUnitChange(unit: string) {
+    this.outputUnit.set(unit)
+  }
+  inputValueChange(value: number) {
+    this.inputValue.set(value)
+
+  }
+
 
 }
